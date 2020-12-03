b827f4f7a04d44bb066fc6a2708f3f8bb2ad27a9
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-03 19:01:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
-// Add your functions and code here
+function destructivelyAppendKitten(Ralp){
+kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(Bob){
+  kittens.unshift("Bob")
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
+}
+
+function appendKitten(Broom){
+ [...kittens, "Broom"]
+}
+
+function prependKitten(Arnold){
+ ["Arnold", ...kittens];
+}
+
+function removeLastKitten(){
+var removeLastKitten = kittens.slice(0, kittens.length - 1)
+}
+
+function removeFirstKitten(){
+var removeFirstKitten = kittens.slice(1)
+}
